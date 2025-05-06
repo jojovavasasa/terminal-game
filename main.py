c1308a53e770178048cdc031ccc1386b7101feb2
jojovavasasa <106984298+jojovavasasa@users.noreply.github.com>
2025-05-06 10:03:46 +0000

    Now uses variables for the questions

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 
+question = "my name is _____: "
+answer = "jeff"
+
 while True:
     time.sleep(1)
     print("hello") 
     time.sleep(1)
     print("fill in the blank")
     time.sleep(1)
-    user_input = input("my name is _____: ")
-    if user_input == "jeff":
+    user_input = input(question)
+    if user_input == answer:
         time.sleep(1)
         print("good job")
         break
