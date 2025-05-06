ec6df1cc778976c6abd850a0dcadee3f1999e5ac
jojovavasasa <106984298+jojovavasasa@users.noreply.github.com>
2025-05-06 10:01:01 +0000

    First test

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("Hello World!")
+import time
+
+while True:
+    time.sleep(1)
+    print("hello") 
+    time.sleep(1)
+    print("fill in the blank")
+    time.sleep(1)
+    user_input = input("my name is _____: ")
+    if user_input == "jeff":
+        time.sleep(1)
+        print("good job")
+        break
+    else:
+        time.sleep(1)
+        print("nope try again")
+    
